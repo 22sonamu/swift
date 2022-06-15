2b679a0866a9f9c667ceda0f005a669a5322fcf8
여정화 <22sonamu@naver.com>
2022-06-15 18:44:37 +0900

    first

---
+++ MyFirstIOS/MyFirstIOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyFirstIOS
+//
+//  Created by 여정화 on 2022/06/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
